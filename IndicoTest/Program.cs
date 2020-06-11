4d170aa254de17c033e64c9b1a71b4d222c41142
Tori Holmess-Kirk <tori.kirk@gmail.com>
2020-06-11 08:47:27 +0200

    Add project files.

---
+++ IndicoTest/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Indico;
+using Indico.Entity;
+using Indico.Request;
+using Newtonsoft.Json.Linq;
+
+namespace IndicoTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            IndicoConfig config = new IndicoConfig(host: "app.indico.io");
+            IndicoClient client = new IndicoClient(config);
+
+            getDatasets(client);
+            getModelGroup(client, 4415);
+            
+        }
+
+        static void getModelGroup(IndicoClient client, int mgId)
+        {
+            ModelGroup mg = client.ModelGroupQuery(mgId).Exec();
+            Console.WriteLine(mg.ToString());
+        }
+
+        static void getDatasets(IndicoClient client)
+        {
+            string query = @"
+                query GetDatasets {
+                    datasets {
+                        id
+                        name
+                        status
+                        rowCount
+                        numModelGroups
+                        modelGroups {
+                            id
+                        }
+                    }
+                  }
+                ";
+
+            GraphQLRequest request = client.GraphQLRequest(query, "GetDatasets");
+            JObject response = request.Call();
+            Console.WriteLine(response);
+        }
+    }
+}
